d5651541ef7fcd80471118cfb2156543bf185f4c
Provod228 <tulush.ignat@bk.ru>
2025-01-30 19:40:08 +0300

    add more functions

--- project/views.py
+++ project/views.py
@@ -2,10 +2,13 @@ from fastapi import FastAPI, APIRouter
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse, FileResponse
 from fastapi.templating import Jinja2Templates
-from registration_db import UserRegistration
+from registration_db import *
 from starlette.requests import Request
-import sqlite3
-from file_db import interlayer_db as editing
+from file_db.interlayer_db import *
+from file_db.editing_db import hash_password
+
+
+data_base = 'school.db'
 
 app = FastAPI()
 router = APIRouter(prefix='/project', tags=['Фронтенд'])
@@ -16,9 +19,9 @@ templates = Jinja2Templates(directory='templates')
 
 @router.get('/students', response_class=HTMLResponse)
 async def get_students_html(request: Request):
-    connection = sqlite3.connect('school.db')
+    connection = sqlite3.connect(data_base)
     cursor = connection.cursor()
-    data_test = editing.Role.get(cursor_db=cursor)
+    data_test = Role.get(cursor_db=cursor)
     connection.commit()
     connection.close()
     return templates.TemplateResponse(
@@ -35,35 +38,51 @@ async def get_login():
     return FileResponse("templates/login.html")
 
 
-@router.post("/registration")
-async def post_registration(user: UserRegistration):
-    connection = sqlite3.connect('school.db')
+@router.post("/login")
+async def post_login(user: UserEntrance):
+    connection = sqlite3.connect(data_base)
     cursor = connection.cursor()
-    if (
-            editing.User.get(cursor_db=cursor, email=user.email) == [] and
-            editing.User.get(cursor_db=cursor, login=user.login) == []
-    ):
-        # User.add(
-        #     cursor_db=cursor,
-        #     username=user.username,
-        #     login=user.login,
-        #     email=user.email,
-        #     password=user.password_input,
-        # )
+    # role_user = Role.get(
+    #     cursor_db=cursor,
+    #     id=User.get(cursor_db=cursor, email=user.email, password=hash_password(user.password))[0]['role_id']
+    # )[0]['title']
+    # print(role_user)    # не трогать, это получение роли
+    if User.get(cursor_db=cursor, email=user.email, password=hash_password(user.password)) != []:
+        connection.commit()
+        connection.close()
         print('f')
-        return {"message": "Registration successful"}
+        return {"message": True}
     else:
+        connection.close()
         print('fff')
-        return {"message": "No registration successful"}
-
-
-@app.get("/")
-async def root():
-    return FileResponse("access_true/login.html")
+        return {"message": False}
 
 
 @router.get("/registration")
 async def registration():
     return FileResponse("templates/registration.html")
+
+
+@router.post("/registration")
+async def post_registration(user: UserRegistration):
+    connection = sqlite3.connect(data_base)
+    cursor = connection.cursor()
+    if (
+            User.get(cursor_db=cursor, email=user.email) == [] and
+            User.get(cursor_db=cursor, login=user.login) == []
+    ):
+        User.add(
+            cursor_db=cursor,
+            username=user.username,
+            login=user.login,
+            email=user.email,
+            password=user.password_input,
+        )
+        connection.commit()
+        connection.close()
+        return {"message": True}
+    else:
+        connection.close()
+        return {"message": False}
 
 app.include_router(router)
